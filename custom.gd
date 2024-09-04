73b7e418b02539faeecaeffda981e53172a11119
max-schilling <gedy.dwegib@gmail.com>
2024-09-04 14:34:34 -0500

    Update Sept 4

---
+++ custom.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _on_btn_1_pressed() -> void:
+	$Label.text = ""
+
+func _on_btn_2_pressed() -> void:
+	
+
+
+func _on_btn_work_pressed() -> void:
+	pass # Replace with function body.
