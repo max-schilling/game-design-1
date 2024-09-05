84250f6e89792fb05b3275552f5c7b364ccb1217
max-schilling <gedy.dwegib@gmail.com>
2024-09-05 13:50:16 -0500

    update 1/2 9/5 24

--- custom.gd
+++ custom.gd
@@ -6,8 +6,13 @@ func _on_btn_1_pressed() -> void:
 	$Label.text = ""
 
 func _on_btn_2_pressed() -> void:
-	
-
+	$Label.text = ""
 
 func _on_btn_work_pressed() -> void:
-	pass # Replace with function body.
+	$Label.text = "Hooray"
+
+func _on_btn_4_pressed() -> void:
+	$Label.text = ""
+
+func _on_btn5_pressed() -> void:
+	get_tree().quit()
--- fav_club.gd
+++ fav_club.gd
@@ -1,13 +1,10 @@
 extends Control
 
-
 func _on_btnshow_pressed() -> void:
 	$Label.text = "Football / Jazz Band"
 
-
 func _on_btnclear_pressed() -> void:
 	$Label.text = ""
-
 
 func _on_btnexit_pressed() -> void:
 	get_tree().quit()
