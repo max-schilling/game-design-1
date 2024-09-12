d9ecf6275fafa6f1f33bdea50c565950b0e24f75
max-schilling <gedy.dwegib@gmail.com>
2024-09-12 14:36:56 -0500

    update 1 || 9/12 24

--- prog_76a.gd
+++ prog_76a.gd
@@ -3,7 +3,25 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	var fun = $LineEdit.text
-	var more = fun * 9
-	var yea = more * 12345679
-	$Label2.text = "boo " + str(yea)
+	var num = $LineEdit.text
+	var num3 = $LineEdit2.text
+	
+	if num3 == 9:
+		$Label2.text = "good"
+		
+	else: num3 > 9
+	$Label2.text = "Uh oh"
+		
+	if num3 == 9:
+		$Label2.text = "good" 
+	else: num3 < 9
+	$Label2.text = "uh oh"
+	
+	var num2 = num * num3
+	var yea = num2 * 12345679
+	
+	$Label2.text = " " + str(yea)
+	
+
+func _on_btn_exit_pressed() -> void:
+	get_tree().quit()
