6386b3adaa5dcf9cf0d1bc7985b37679c5c1bae6
max-schilling <gedy.dwegib@gmail.com>
2024-09-20 14:35:22 -0500

    update || 9/20

--- prog_130a.gd
+++ prog_130a.gd
@@ -3,17 +3,21 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	for num in range(0 , 51, 2):
-		(num * 3) + 1
-		var line = str(num)
-		$ItemList.add_item(line)
-	for num in range(1, 52, 2):
-		(num / 2)
-		var crocant = str(num)
+	var bum = $LineEdit.text
+	$ItemList.add_item(bum)
+	for num in range(1 , 52, 2):
+		if bum == $LineEdit.text:  
+			var car = (num * 3) + 1
+			var line = str(car)
+			$ItemList.add_item(line)
+		
+	for num in range(0, 51, 2):
+		var mar = (num / 2)
+		var crocant = str(mar)
 		$ItemList.add_item(crocant)
 
 func _on_button_2_pressed() -> void:
-	$ItemList.text = ""
+	$ItemList.clear()
 
 
 func _on_button_3_pressed() -> void:
