524fefb1941dc9b262b3f0696ee9fe20afb5edd6
max-schilling <gedy.dwegib@gmail.com>
2024-09-24 14:36:21 -0500

    update || 9/24

--- LangDocs/prog_130a.gd
+++ LangDocs/prog_130a.gd
@@ -3,18 +3,17 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	var bum = $LineEdit.text
-	$ItemList.add_item(bum)
-	while bum in range(1, 52, 2):
+	var bum = int($LineEdit.text)
+	var um = str(bum)
+	$ItemList.add_item(um)
+	while um in range(5, 52, 2):
 		var boat = (bum * 3) + 1
 		$ItemList.add_item(boat)
-	while bum in range(0, 51, 2):
-		var rum = bum / 2
+		$ItemList.text = "" + boat
+	while um in range(0, 51, 2):
+		var rum = str(bum / 2)
 		$ItemList.add_item(rum)
-	if bum < 5:
-		$ItemList.add_Item()
-	else:
-		$ItemList.add_item()
+		$ItemList.text = "" + rum
 	
 func _on_button_2_pressed() -> void:
 	$ItemList.clear()
