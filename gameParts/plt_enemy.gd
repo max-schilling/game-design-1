1983e36d8fd4220ba29efbc9772471d7473ae5a9
max-schilling <gedy.dwegib@gmail.com>
2024-10-08 14:36:47 -0500

    update 10/8

--- gameParts/plt_enemy.gd
+++ gameParts/plt_enemy.gd
@@ -3,11 +3,13 @@ extends RigidBody2D
 var speed = 100
 var direction = -1
 
+
 func _ready() -> void:
 	$Timer.start()
 	
 func _physics_process(delta: float) -> void:
 	self.linear_velocity.x = speed * direction
+	
 
 func _on_timer_timeout() -> void:
 	direction *= -1
@@ -17,9 +19,9 @@ func _on_body_entered(body: Node) -> void:
 		body.queue_free()
 		OS.alert("You died")
 		get_tree().reload_current_scene()
-
-update_animation(direction)
+	
+	update_animation(direction)
 
 @onready var anim = $AnimatedSprite2D
 func update_animation(direction):
-	anim.play(default)
+	anim.play("default")
