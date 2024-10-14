3400717913782bf99923e85f80ba73a7fecc03c1
Max Schilling <gedy.dwegib@gmail.com>
2024-10-13 20:34:42 -0500

    fixed portal thing

--- gameParts/change_lvl.gd
+++ gameParts/change_lvl.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
 
-@export var next_level = ""
+@export var next_level = "plt_world_2"
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	var groups = body.get_groups()
+	for aGroup in groups:
+		print(aGroup)
+	#if body.is_in_group("player"):
+	if body.name == "plt_player":
 		if next_level == "":
 			OS.alert("You've run dry of levels")
 		else:
-			var lvl = "res://" + next_level + ".tscn"
+			var lvl = "res://gameParts/" + next_level + ".tscn"
 			get_tree().change_scene_to_file(lvl)
