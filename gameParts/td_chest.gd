f2e0c7808de259eb01185f5431f8c1eb13a07e95
max-schilling <gedy.dwegib@gmail.com>
2024-12-02 14:34:48 -0600

    update || 12/2

--- gameParts/td_chest.gd
+++ gameParts/td_chest.gd
@@ -1,24 +1,11 @@
-extends Node2D
+extends BaseItem
 
-@export var next_level = "td_world_2" 
+func update():
+	flicker_start_time == 3.0
+	time_to_despawn == 6.0
 
-var drops = ["drop_coin", "drop_heart"]
-@onready var audplay = $AudioStreamPlayer2D
-var coin_scene = preload("res://entites/items/mini_coin.tscn")
-var heart_scene = preload("res://entites/items/mini_mart.tscn")
-var damage_shader = preload("res://assets/shaders/take_damage.tres")
-func drop_items():
-	var num_drops = randi() % 3 + 1
-	for i in range(num_drops):
-		var rnd_drop = drops[randi() % drops.size()]
-		call_deferred(rnd_drop)
+func _init(): super._init(5)
 
-func _on_body_entered(body: Node2D) -> void:
-	var groups = body.get_groups()
-	for aGroup in groups:
-		print(aGroup)
-	
-	if body.name == "td_player":
-		drop_items()
-		audplay.play()
-		
+func interact(player):
+	player.pickup_money(value)
+	player.pickup_health(value)
