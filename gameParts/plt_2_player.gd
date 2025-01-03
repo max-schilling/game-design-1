62684b8c24d445287880a69784f1a689930f71d8
max-schilling <gedy.dwegib@gmail.com>
2025-01-03 14:34:25 -0600

    update ||1/3

--- gameParts/plt_2_player.gd
+++ gameParts/plt_2_player.gd
@@ -3,19 +3,18 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
-
+@onready var anim = $AnimatedSprite2D
+@onready var aud_p = $AudioStreamPlayer2D
+func get_direction_name():
+	return ["right", "left"]
+var dir = get_direction_name()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +22,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func update_animation(dir):
+	if not self.is_on_floor:
+		anim.play("jump_" + dir)
+	elif dir != 0:
+		anim.play("walk_" + dir)
+		anim.flip_h = dir < 0
+	else:
+		anim.play("idle_" + dir)
