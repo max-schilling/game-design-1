69b4124e84bcd8ac6b13330c0d68f63037940808
max-schilling <gedy.dwegib@gmail.com>
2025-01-23 11:53:42 -0600

    FINAL CLASS UPDATE || 1/23/24

--- gameParts/plt_2player.gd
+++ gameParts/plt_2player.gd
@@ -4,13 +4,27 @@ extends CharacterBody2D
 var SPEED = 200.0
 var JUMP_VELOCITY = -300.0
 @onready var anim = $AnimatedSprite2D
-
+@onready var sound = $AudioStreamPlayer2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		anim.play("jump")
+	if not is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		velocity.y = JUMP_VELOCITY
+	if not is_on_floor() and Input.is_action_just_pressed("dash"):
+		var direction := Input.get_axis("ui_left", "ui_right")
+		velocity.x = -JUMP_VELOCITY * 5
+		if direction < 0:
+			velocity.x = JUMP_VELOCITY * 5
+		anim.play("dash")
+		sound.play()
+		await sound.finished
+	if not is_on_floor():
+		pass
+	if is_on_wall():
+		velocity / 2
 	if is_on_floor():
 		if velocity.x != 0:
 			anim.play("walk_1")
